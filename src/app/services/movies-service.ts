0b580a7372f6d7898d736fb297a0340d41e494b0
Max Kilovatiy <maxkvt@gmail.com>
2018-11-30 17:25:00 +0300

    Adjust code style

--- src/app/services/movies-service.ts
+++ src/app/services/movies-service.ts
@@ -1,39 +1,43 @@
-import { Movie } from '../model/movie';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { Subject } from 'rxjs/internal/Subject';
-import { of } from 'rxjs/internal/observable/of';
+import { Movie } from '../model/movie'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs/internal/Observable'
+import { Subject } from 'rxjs/internal/Subject'
+import { of } from 'rxjs/internal/observable/of'
 
-import { movies } from '../../data/movie.mock-data';
+import { movies } from '../../data/movie.mock-data'
 
 @Injectable()
 export class MoviesService {
-
   get movies$(): Observable<Movie[]> {
-    return this.movies.asObservable();
+    return this.movies.asObservable()
   }
 
-  private movies = new Subject<Movie[]>();
+  private movies = new Subject<Movie[]>()
 
-  constructor() {
-
-  }
+  constructor() {}
 
   getMovies(): void {
-    this.movies.next(movies.map(item => {
-      return new Movie(item);
-    }));
+    this.movies.next(
+      movies.map(item => {
+        return new Movie(item)
+      })
+    )
   }
 
   refreshMovies(items: Movie[]): void {
-    this.movies.next(items);
+    this.movies.next(items)
   }
 
   getMovie(id: number): Observable<Movie> {
-    return of(movies.filter(i => i.id === id).map(item => {
-      return new Movie(item);
-    }).reduce((p, k) => {
-      return k;
-    }, null));
+    return of(
+      movies
+        .filter(i => i.id === id)
+        .map(item => {
+          return new Movie(item)
+        })
+        .reduce((p, k) => {
+          return k
+        }, null)
+    )
   }
 }
